576aede1b5e94317cabc9bb155f0c43c9f08c942
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2026-02-26 17:24:24 +0530

    push

---
+++ src/pages/LangraphAgent.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom"
+
+export default function LangraphAgent() {
+    const location = useLocation()
+
+  return (
+    <>
+      <div>Coming soon</div>
+    </>
+  );
+}
